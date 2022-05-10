b924ac8c47d26a41fdba617e486551e21e2da603
PaulosPatilas <pavlossock@gmail.com>
2022-05-10 10:24:41 +0300

    proxy-middleware fixed

--- ReactProject/my-app/src/setupProxy.js
+++ ReactProject/my-app/src/setupProxy.js
@@ -2,19 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware('/api/*',{
-        target: 'http://localhost:8080',
-        changeOrigin: true
-    })
-  );
-  app.use(
-    createProxyMiddleware('/api/*/*',{
-        target: 'http://localhost:8080',
-        changeOrigin: true
-    })
-  );
-  app.use(
-    createProxyMiddleware('/employeeupd/api/*/*',{
+    createProxyMiddleware('/api/**',{
         target: 'http://localhost:8080',
         changeOrigin: true
     })
